ac2ef014db420a24fb45d7012c3c4a6094825f93
HeberC2003 <00054621@uca.edu.sv>
2026-05-21 19:07:15 -0600

    feat: add Material model

--- labo1/src/main/java/org/example/labo1/model/Material.java
+++ labo1/src/main/java/org/example/labo1/model/Material.java
@@ -1,4 +1,16 @@
 package org.example.labo1.model;
 
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Material {
+    private String name;
+    private Category category;
+    private CookingEffect cookingEffect;
+    private Double price;
+    private String location;
+    private Rarity rarity;
 }
